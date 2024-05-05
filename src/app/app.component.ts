b1a3073d3e5c670268ea7b86730df5e8b0c43e5d
vilarj <lamaravillasan@outlook.com>
2024-05-05 11:14:56 -0400

    Reinventing the project's vision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { UserComponent } from './user/UserComponent';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, UserComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
