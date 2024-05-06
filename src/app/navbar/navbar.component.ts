272d74b5e19c6cc121e225677b5d4fa4828057cd
vilarj <lamaravillasan@outlook.com>
2024-05-06 00:31:35 -0400

    Continue working on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FavoritesComponent } from '../favorites/favorites.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FavoritesComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  LINK: string = 'https://www.youtube.com/@vilarj';
+  pages: [string, string] = ['/books/', ''];
+}
