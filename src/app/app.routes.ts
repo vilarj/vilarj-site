bf625e7ef5f92bdd5869c2776709f7c0276de221
vilarj <lamaravillasan@outlook.com>
2024-05-08 20:24:21 -0400

    Creating a page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { FavoriteGamesComponent } from './favorites/favorite-games/favorite-games.component';
 import { FavoriteLiteratureComponent } from './favorites/favorite-literature/favorite-literature.component';
 import { FavoriteMoviesComponent } from './favorites/favorite-movies/favorite-movies.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: '**', component: PageNotFoundComponent },
   {
     path: 'app-favorite-games',
     redirectTo: '/favorite-games',
